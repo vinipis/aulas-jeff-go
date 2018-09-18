4ab5c7f60ff28e203395d5e9b7a3d60f29708708
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2018-09-18 10:55:42 -0300

    ajuste no gobuild com uma explicação melhorada

--- fundamentos/gobuild/main.go
+++ fundamentos/gobuild/main.go
@@ -3,6 +3,8 @@ package main
 //GOOS=windows GOARCH=386 go build -o meuapp.exe
 //GOOS=linux GOARCH=arm go build -o meuappraspberry -v github.com/jeffprestes/cursodego/gobuild
 
+//go build ou go build -o nomedoarquivo resolvemelhor que os de cima
+
 func main() {
 
 }
